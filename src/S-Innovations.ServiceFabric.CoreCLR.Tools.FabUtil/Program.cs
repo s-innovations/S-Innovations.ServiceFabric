4eeee7d1678b85aee33f7a2d9a1c5e4b8e1d3215
Poul Kjeldager Sørensen <pks@s-innovations.net>
2017-01-07 23:35:25 +0100

    added a tool test

---
+++ src/S-Innovations.ServiceFabric.CoreCLR.Tools.FabUtil/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SInnovations.ServiceFabric.CoreCLR.Tools.FabUtil
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World");
+        }
+    }
+}
